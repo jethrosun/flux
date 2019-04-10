77243c02ef7c278d9e2a0c695f370551eb2deb76
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-04-10 13:14:44 -0400

    Checkpoint: install necessary pkg and setup python env

--- install.sh
+++ install.sh
@@ -2,3 +2,26 @@
 
 sudo apt install -y python-pip python-matplotlib
 sudo apt install -y automake
+
+sudo apt install -y libreadline6 libreadline6-dev
+sudo apt install -y bzip2 
+sudo apt install -y libsqlite3-dev
+
+# Set version to 2.7.10
+export PYENV_VERSION=3.7.3
+
+pyenv install $PYENV_VERSION
+pip install -r requirements.txt
+
+# Reset version
+unset PYENV_VERSION
+
+# Set version to 2.7.10
+export PYENV_VERSION=2.7.16
+
+
+pyenv install $PYENV_VERSION
+
+# Reset version
+unset PYENV_VERSION
+
