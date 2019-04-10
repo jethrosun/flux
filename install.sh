ed2e2d9f0312a02a9af441c6b978af29a4870999
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-04-10 13:18:15 -0400

    Just some simple update

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 sudo apt install -y python-pip python-matplotlib
 sudo apt install -y automake
 
-sudo apt install -y libreadline6 libreadline6-dev
-sudo apt install -y bzip2 
-sudo apt install -y libsqlite3-dev
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+sudo apt install -y libedit-dev
+
 
 # Set version to 2.7.10
 export PYENV_VERSION=3.7.3
