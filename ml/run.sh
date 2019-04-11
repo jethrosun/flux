941d068ce9e306aeba963e541e0977525dc60b35
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-04-11 08:41:07 -0400

    Just some simple update

--- ml/run.sh
+++ ml/run.sh
@@ -3,12 +3,12 @@
 mkdir -p log
 
 # Set version to 3.7.3
-#export PYENV_VERSION=3.7.3
-#python ffnn.py | tee log/ffnn.log
-#python lstm.py | tee log/lstm.log
+export PYENV_VERSION=3.7.3
+python ffnn.py | tee log/ffnn.log
+python lstm.py | tee log/lstm.log
 
 # Reset version
-#unset PYENV_VERSION
+unset PYENV_VERSION
 
 # Set version to 2.7.16
 export PYENV_VERSION=2.7.16
